814b5e0142b610309b2dadc7144b7c43fe889284
wow13524 <50389483+wow13524@users.noreply.github.com>
2022-04-15 08:40:48 -0700

    Breaking Test #2
    getLinks finds an invalid link with text between [] and ().

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,7 +12,6 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            System.out.println(currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
@@ -25,7 +24,6 @@ public class MarkdownParse {
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
-        System.out.println(currentIndex);
         return toReturn;
     }
 
