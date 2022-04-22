fc6285309f65abcbfd5af028caf00bbbe75bd39e
wow13524 <50389483+wow13524@users.noreply.github.com>
2022-04-22 08:42:42 -0700

    Add unit test for test-file.md

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,21 @@
-import static org.junit.Assert.*;
-import org.junit.*;
-public class MarkdownParseTest {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;   //Imports assert methods from JUnit
+import org.junit.*;                 //Imports everything from JUnit
+public class MarkdownParseTest {    //Declares the MarkdownParseTest class
+    @Test                           //Annotation to mark the method as a JUnit test
+    public void addition() {        //Method representing an addition unit test
+        assertEquals(2, 1 + 1);     //Errors if 2 != 1 + 1, passes otherwise
+    }                               //Close brace
+
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void test_test_file() throws IOException{
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertArrayEquals(new Object[]{"https://something.com","some-thing.html"},links.toArray());
     }
-}
+}                                   //Close brace
